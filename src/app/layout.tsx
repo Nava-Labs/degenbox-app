147b442aca2e57ac9a2a3a88a195c15665f2c405
xerod <xerod@outlook.com>
2024-11-16 02:50:33 +0700

    style: implement custom fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,47 @@
-import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from '../config';
+import type { Metadata } from "next";
+import { NEXT_PUBLIC_URL } from "../config";
 
-import './global.css';
-import '@coinbase/onchainkit/styles.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import dynamic from 'next/dynamic';
+import "./global.css";
+import "@coinbase/onchainkit/styles.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { Playpen_Sans } from "next/font/google";
+import dynamic from "next/dynamic";
+
+//👇 Configure our font object
+const playpenSans = Playpen_Sans({
+  subsets: ["latin"],
+  variable: "--font-opensans",
+});
 
 const OnchainProviders = dynamic(
-  () => import('src/components/OnchainProviders'),
+  () => import("src/components/OnchainProviders"),
   {
     ssr: false,
   },
 );
 
 export const viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1.0,
 };
 
 export const metadata: Metadata = {
-  title: 'Onchain App Template',
-  description: 'Built with OnchainKit',
+  title: "Onchain App Template",
+  description: "Built with OnchainKit",
   openGraph: {
-    title: 'Onchain App Template',
-    description: 'Built with OnchainKit',
+    title: "Onchain App Template",
+    description: "Built with OnchainKit",
     images: [`${NEXT_PUBLIC_URL}/vibes/vibes-19.png`],
   },
 };
 
 export default function RootLayout({
   children,
-}: { children: React.ReactNode }) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
+    <html lang="en" className={playpenSans.className}>
       <body className="flex items-center justify-center">
         <OnchainProviders>{children}</OnchainProviders>
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
   content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-opensans)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
