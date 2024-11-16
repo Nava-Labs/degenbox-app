2c1d6c7f6a8d23b45dfdce152de4e67029388668
xerod <xerod@outlook.com>
2024-11-17 06:03:34 +0700

    fix: improve transaction modal

--- src/components/TransactionModal.tsx
+++ src/components/TransactionModal.tsx
@@ -1,47 +1,54 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import { DialogDescription } from '@/components/ui/dialog';
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
-import { ExternalLink } from "lucide-react";
-import Link from "next/link";
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+} from '@/components/ui/drawer';
+import { ExternalLink } from 'lucide-react';
+import Link from 'next/link';
+import Spinner from '@/public/spinner.svg';
+import { useEffect, useState } from 'react';
 
-export function TransactionModal(txHash: string) {
+export function TransactionModal({ txHash }: { txHash?: `0x${string}` }) {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (txHash) {
+      setOpen(true);
+    }
+  }, [txHash]);
+
   return (
-    <Dialog open>
-      {/* <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger> */}
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerContent className="sm:max-w-[425px]">
+        <DrawerHeader>
           <h1 className="font-black text-center text-4xl h4-shadow text-primary-200 leading-none">
             Processing Transaction
           </h1>
-          <DialogTitle></DialogTitle>
-          <DialogDescription>
+          <DialogDescription className="font-bold mx-6">
             You can leave the page if you want. The transaction will keep
             processing.
           </DialogDescription>
-        </DialogHeader>
-        <img src="/spinner.svg" className="flex h-32 w-32 mx-auto" />
-        <DialogFooter className="flex gap-2">
-          <Link
-            href={`https://base.blockscout.com/tx/${txHash}`}
-            target="_blank"
-          >
-            <Button>
-              Check transaction on explorer{" "}
+        </DrawerHeader>
+        <Spinner className="flex h-32 w-32 mx-auto text-primary-300 my-8" />
+        <DrawerFooter className="flex items-center p-2">
+          <Link href={`https://www.cctpscan.xyz/tx/${txHash}`} target="_blank">
+            <Button className="text-lg">
+              Check transaction on explorer{' '}
               <ExternalLink className="w-10 h-10" />
             </Button>
           </Link>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+          <Button
+            className="text-lg text-primary-600 h-10"
+            variant={'link'}
+            onClick={() => setOpen(false)}
+          >
+            Close
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
