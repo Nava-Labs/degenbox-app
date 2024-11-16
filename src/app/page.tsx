d1eb91d533ecbc1d8c67086c3c341e74c04e6fd3
xerod <xerod@outlook.com>
2024-11-16 16:45:41 +0700

    style: fix header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
               alt="Rainbow header"
             />
 
-            <div className="absolute top-0 left-0 right-0 flex flex-col items-center w-full pt-14">
+            <div className="absolute top-0 left-0 right-0 flex flex-col items-center w-full pt-10">
               <div className="flex px-3 py-0.5 items-center justify-center rounded-full text-xs font-black bg-[#C2E6F5] border border-b-4 border-l-2 border-r-2 border-primary-900 text-center text-primary-900 shadow-sm">
                 Welcome, navalabs.base.eth 👋🏼
               </div>
@@ -37,29 +37,34 @@ export default function Page() {
                   Boxes
                 </h1>
               </div>
+            </div>
 
-              <div className="flex py-0.5 px-3 items-center justify-center rounded-full text-md font-medium bg-primary-100 border-4 border-white text-center text-primary-900 shadow-sm mt-2">
-                <div className="flex justify-center gap-4">
-                  <div className="flex items-center gap-1">
+            <div className="absolute -bottom-[22px] w-full items-center justify-center">
+              <div className="flex w-fit mx-auto pl-1 rounded-full text-md font-medium bg-primary-100 border-4 border-white text-center text-primary-900">
+                <div className="flex justify-center gap-1">
+                  <div className="flex items-center gap-0.5">
                     <img
-                      src="/icons/ethereum.svg"
-                      className="h-3 w-3"
-                      alt="Ethereum icon"
+                      src="/icons/dollar.svg"
+                      className="w-5 h-5"
+                      alt="Dollar icon"
                     />
-                    <span className="text-primary-700 font-bold text-xs">
-                      69 ETH
+                    <span className="text-primary-700 font-bold text-sm">
+                      199 USDC
                     </span>
                   </div>
 
-                  <div className="flex items-center gap-1">
-                    <img
-                      src="/icons/box.svg"
-                      className="h-3 w-3"
-                      alt="Box icon"
-                    />
-                    <span className="text-primary-700 font-bold text-xs">
-                      420 ETH
-                    </span>
+                  <div className="flex py-1 px-2.5 items-center justify-center rounded-full text-md font-medium bg-primary-400 text-center text-white">
+                    <div className="flex justify-center gap-4">
+                      <div className="flex items-center gap-1">
+                        <span className="font-bold text-sm">Top Up</span>
+
+                        <img
+                          src="/icons/plus.svg"
+                          className="w-4 h-4"
+                          alt="Plus icon"
+                        />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
