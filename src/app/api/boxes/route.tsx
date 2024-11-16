882091db078244e64039f6bf0de230c94713028a
xerod <xerod@outlook.com>
2024-11-17 00:25:49 +0700

    feat: implement fetch

--- src/app/api/boxes/route.tsx
+++ src/app/api/boxes/route.tsx
@@ -1,40 +1,44 @@
-import { NextResponse } from 'next/server'
-import { PriceServiceConnection } from '@pythnetwork/price-service-client'
-import { createClient } from '@supabase/supabase-js'
+import { NextResponse } from 'next/server';
+import { PriceServiceConnection } from '@pythnetwork/price-service-client';
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
 
-const priceServiceConnection = new PriceServiceConnection('https://hermes.pyth.network', { priceFeedRequestConfig: { binary: true } }
-)
+const priceServiceConnection = new PriceServiceConnection(
+  'https://hermes.pyth.network',
+  { priceFeedRequestConfig: { binary: true } },
+);
 
 export async function GET(request: Request) {
   // const { searchParams } = new URL(request.url)
   // Get box data from Supabase
-  let { data: boxes, error } = await supabase
-    .from('boxes')
-    .select('*')
-    // .limit(1)
-    // .single()
+  let { data: boxes, error } = await supabase.from('boxes').select('*');
+  // .limit(1)
+  // .single()
   if (error || !boxes) {
-    return NextResponse.json({ error: 'Box not found', message: error }, { status: 404 })
+    return NextResponse.json(
+      { error: 'Box not found', message: error },
+      { status: 404 },
+    );
   }
   // console.log("boxes.length ", boxes.length)
   try {
     // Get all prices in parallel
     for (let i = 0; i < boxes.length; i++) {
-    // for (let i = 0; i < 1; i++) {
+      // for (let i = 0; i < 1; i++) {
       let box = boxes[i];
       // let pricePromises = [];
-      let feed_ids = []
+      let feed_ids = [];
       let tokenListLength = box.token_list.length;
       for (let j = 0; j < tokenListLength; j++) {
-        feed_ids.push(box.token_list[j].feed_id)
+        feed_ids.push(box.token_list[j].feed_id);
       }
       // console.log("feed id", feed_ids)
-      const priceFeeds = await priceServiceConnection.getLatestPriceFeeds(feed_ids);
+      const priceFeeds =
+        await priceServiceConnection.getLatestPriceFeeds(feed_ids);
       // console.log("price feeeeeedds ", priceFeeds)
       if (priceFeeds) {
         for (let j = 0; j < tokenListLength; j++) {
@@ -42,24 +46,39 @@ export async function GET(request: Request) {
           // Convert price to actual value and then to decimal 18
           // Current: price.price * 10^(price.expo) -> actual price
           // Then multiply by 10^18 for desired decimals
-          let normalizedPrice = (BigInt(price.price) * BigInt(10 ** (18 + Number(price.expo)))).toString();
+          let normalizedPrice = (
+            BigInt(price.price) * BigInt(10 ** (18 + Number(price.expo)))
+          ).toString();
           boxes[i].token_list[j].pricePerToken = normalizedPrice;
-          boxes[i].token_list[j].totalUSDValue = (BigInt(normalizedPrice) * BigInt(boxes[i].token_list[j].amount) / BigInt(10 ** 18)).toString();
+          boxes[i].token_list[j].totalUSDValue = (
+            (BigInt(normalizedPrice) * BigInt(boxes[i].token_list[j].amount)) /
+            BigInt(10 ** 18)
+          ).toString();
           if (boxes[i].token_list[j].totalUSDValue) {
-            const formattedUSDValue = BigInt(boxes[i].token_list[j].totalUSDValue);
-            boxes[i].token_list[j].formattedUSDValue = (Number(formattedUSDValue) / 10 ** 18).toString();
+            const formattedUSDValue = BigInt(
+              boxes[i].token_list[j].totalUSDValue,
+            );
+            boxes[i].token_list[j].formattedUSDValue = (
+              Number(formattedUSDValue) /
+              10 ** 18
+            ).toString();
           }
 
           if (boxes[i].token_list[j].pricePerToken) {
-            const formattedPricePerToken = BigInt(boxes[i].token_list[j].pricePerToken);
-            boxes[i].token_list[j].formattedPricePerToken = (Number(formattedPricePerToken) / 10 ** 18).toString();
+            const formattedPricePerToken = BigInt(
+              boxes[i].token_list[j].pricePerToken,
+            );
+            boxes[i].token_list[j].formattedPricePerToken = (
+              Number(formattedPricePerToken) /
+              10 ** 18
+            ).toString();
           }
         }
         let boxPrice = BigInt(0);
         for (let j = 0; j < tokenListLength; j++) {
           let tokenAmount = BigInt(boxes[i].token_list[j].amount);
           let tokenPrice = BigInt(boxes[i].token_list[j].pricePerToken);
-          boxPrice += (tokenAmount * tokenPrice)/ BigInt(10 ** 18);
+          boxPrice += (tokenAmount * tokenPrice) / BigInt(10 ** 18);
         }
         boxes[i].boxPrice = boxPrice.toString();
       }
@@ -69,20 +88,20 @@ export async function GET(request: Request) {
         const rawPrice = BigInt(boxes[i].boxPrice);
         boxes[i].formattedBoxPrice = (Number(rawPrice) / 10 ** 18).toString();
       }
-
     }
 
-    console.log("boxes ", boxes)
+    console.log('boxes ', JSON.stringify(boxes, null, 2));
+    console.log({ boxes, token: boxes[0].token_list });
     // console.log("boxes ", boxes[0].token_list)
 
     return NextResponse.json({
-      boxes: boxes
-    })
+      boxes: boxes,
+    });
   } catch (error) {
-    console.error('Error fetching prices:', error)
+    console.error('Error fetching prices:', error);
     return NextResponse.json(
       { error: 'Failed to fetch prices' },
-      { status: 500 }
-    )
+      { status: 500 },
+    );
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,91 @@
-"use client";
+'use client';
 
-import { StickyBottomNavbar } from "@/components/sticky-bottom-navbar";
-import { useAccount } from "wagmi";
-import Onboarding from "../components/Onboarding";
-import BoxIcon from "@/public/icons/box.svg";
-import { Button } from "@/components/ui/button";
-import TopUpButton from "../components/TopUpButton";
+import { StickyBottomNavbar } from '@/components/sticky-bottom-navbar';
+import { useAccount } from 'wagmi';
+import { useQuery } from '@tanstack/react-query';
+import Onboarding from '../components/Onboarding';
+import BoxIcon from '@/public/icons/box.svg';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import TopUpButton from '../components/TopUpButton';
+import { formatEther } from 'ethers';
+import { truncateAddress } from '../lib/utiles';
+import Loading from '@/public/loading.svg';
+import RainbowHeader from '@/public/rainbow-header.svg';
+
+// Type definition for Box
+type Box = {
+  id: number;
+  name: string;
+  token_list: Token[] | [];
+  address: string;
+  boxPrice: string;
+  formattedBoxPrice: string;
+};
+
+type Token = {
+  name: string;
+  amount: string;
+  address: string;
+  feed_id: string;
+  logo_url: string;
+  domain_id: string;
+  pricePerToken: string;
+  totalUSDValue: string;
+  formattedUSDValue: string;
+  formattedPricePerToken: string;
+};
+
+// Type for CCTP Domain Map
+type CctpDomainMap = {
+  [key: number]: string;
+};
 
 export default function Page() {
   const { address } = useAccount();
 
+  const {
+    data: boxes,
+    isLoading,
+    error,
+  } = useQuery<Box[]>({
+    queryKey: ['boxes'],
+    queryFn: async () => {
+      const response = await axios.get('/api/boxes/');
+      return response.data.boxes;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div className="min-h-[80vh] flex items-center justify-center bg-primary-50">
+        <Loading className="w-24 h-24 text-primary-200" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error fetching boxes</div>;
+  }
+
   return (
     <div>
-      {!address ? (
-        <Onboarding />
-      ) : (
+      {address ? (
         <>
           <Heading />
           <div className="px-6 w-full min-h-full pt-8 pb-20 bg-white">
             <div className="flex space-x-2.5 mb-2">
-              <div className="w-full rounded-full h-2 bg-primary-300" />
-              <div className="w-full rounded-full h-2 bg-primary-300 opacity-25" />
-              <div className="w-full rounded-full h-2 bg-primary-300 opacity-25" />
+              <div className="w-full rounded-full h-1.5 bg-primary-300" />
+              <div className="w-full rounded-full h-1.5 bg-primary-300 opacity-25" />
+              <div className="w-full rounded-full h-1.5 bg-primary-300 opacity-25" />
             </div>
-            <BoxList />
+            {!boxes && <li>No Boxes Found</li>}
+            {!!boxes && <BoxList box={boxes[0]} tokens={boxes[0].token_list} />}
           </div>
           <StickyBottomNavbar />
         </>
+      ) : (
+        <Onboarding />
       )}
     </div>
   );
@@ -36,7 +95,7 @@ function Heading() {
   const { address } = useAccount();
   return (
     <div className="w-full relative">
-      <img src="/rainbow-header.svg" className="w-full" alt="Rainbow header" />
+      <RainbowHeader className="w-full" />
 
       <div className="absolute top-[10%] left-0 right-0 flex flex-col items-center w-full">
         <div className="flex px-3 py-0.5 items-center justify-center rounded-full text-xs font-black bg-[#C2E6F5] border border-b-4 border-l-2 border-r-2 border-primary-900 text-center text-primary-900 shadow-sm">
@@ -79,96 +138,61 @@ function Heading() {
   );
 }
 
-const data = [
-  {
-    name: "POPCAT",
-    id: "0xbF8A...C637",
-    percentage: 50,
-    amount: "4.51M",
-    domain_id: 5,
-    icon_url: "https://s2.coinmarketcap.com/static/img/coins/64x64/28782.png", // Siamese cat icon with purple badge
-  },
-  {
-    name: "MOG",
-    id: "0xbF8A...C637",
-    percentage: 12.5,
-    amount: "4.52M",
-    domain_id: 0,
-    icon_url: "https://s2.coinmarketcap.com/static/img/coins/64x64/27659.png", // Blue and yellow cat icon
-  },
-  {
-    name: "SPX",
-    id: "0xbF8A...C637",
-    percentage: 11.5,
-    amount: "4.52M",
-    domain_id: 0,
-    icon_url: "https://s2.coinmarketcap.com/static/img/coins/64x64/28081.png", // Yellow circular logo
-  },
-  {
-    name: "BITCOIN",
-    id: "0xbF8A...C637",
-    percentage: 11.5,
-    amount: "4.52M",
-    domain_id: 0,
-    icon_url: "https://s2.coinmarketcap.com/static/img/coins/64x64/25220.png", // Retro-style pixel art icon
-  },
-  {
-    name: "BRETT",
-    id: "0xbF8A...C637",
-    percentage: 14.5,
-    amount: "4.52M",
-    domain_id: 6,
-    icon_url: "https://s2.coinmarketcap.com/static/img/coins/64x64/29743.png", // Blue pepe-style icon
-  },
-];
-
-function BoxList() {
-  const cctpDomainMap = {
-    0: "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
-    5: "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png",
-    6: "https://s2.coinmarketcap.com/static/img/coins/64x64/27716.png",
+function BoxList({ box, tokens }: { box: Box; tokens: Token[] }) {
+  const cctpDomainMap: CctpDomainMap = {
+    0: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+    5: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png',
+    6: 'https://s2.coinmarketcap.com/static/img/coins/64x64/27716.png',
   };
 
   return (
     <div className="relative w-full border border-primary-100 rounded-lg px-3 py-2.5">
       {/* Card Header */}
       <div className="w-full flex justify-between items-start">
-        <div className="flex items-start gap-2">
-          <div className="bg-primary-200 size-12 flex items-center justify-center rounded-lg overflow-hidden">
-            <BoxIcon
-              src="/icons/box.svg"
-              className="w-8 h-8 stroke-primary-600"
-              alt="Box icon"
-            />
+        <div className="flex w-full flex-col items-start gap-2">
+          <div className="flex w-full justify-between items-center">
+            <div className="bg-primary-200 size-12 flex items-center justify-center rounded-lg overflow-hidden">
+              <BoxIcon
+                src="/icons/box.svg"
+                className="w-8 h-8 stroke-primary-600"
+                alt="Box icon"
+              />
+            </div>
+            <div className="text-right">
+              <h1 className="text-primary-800 text-lg font-bold leading-none">
+                ${box.formattedBoxPrice}
+              </h1>
+              <h1 className="text-primary-300 text-lg font-bold leading-none mt-1">
+                per box
+              </h1>
+            </div>
           </div>
           <div>
-            <h1 className="text-primary-800 text-lg font-bold">Boxes</h1>
+            <h1 className="text-primary-800 text-lg font-bold">{box.name}</h1>
             <p className="text-primary-300 text-sm font-bold">
-              1000+ Purchased Boxes
+              {tokens?.length || 0} Purchased Boxes
             </p>
           </div>
-        </div>
-        <div className="text-right">
-          <h1 className="text-primary-800 text-lg font-bold leading-none">
-            $130
-          </h1>
-          <h1 className="text-primary-300 text-lg font-bold leading-none mt-1">
-            per box
-          </h1>
         </div>
       </div>
 
       {/* Card Body */}
       <ul className="mt-5 space-y-4">
-        {data.map((item) => (
-          <li className="flex items-center justify-between">
+        {tokens.map((item) => (
+          <li key={item.address} className="flex items-center justify-between">
             <div className="flex items-center gap-2">
               <div className="w-8 h-8 relative overflow-hidden">
                 {/* Meme Logo */}
-                <img src={item.icon_url} className="rounded-full" />
+                <img
+                  aria-label={`${item.name} Logo`}
+                  src={item.logo_url}
+                  className="rounded-full"
+                />
                 {/* Network */}
                 <img
+                  // biome-ignore lint/suspicious/noExplicitAny: Can't provide types for now
                   src={(cctpDomainMap as any)[item.domain_id]}
+                  alt="Network"
                   className="w-3 h-3 rounded-full absolute right-0 bottom-0 outline outline-2 outline-white"
                 />
               </div>
@@ -177,29 +201,29 @@ function BoxList() {
                   {item.name}
                 </p>
                 <p className="text-primary-400 font-bold text-xs mt-1 leading-none">
-                  {item.id}
+                  {truncateAddress(item.address, 5)}
                 </p>
               </div>
             </div>
             <div className="text-right">
               <p className="text-primary-700 font-bold leading-none">
-                {item.percentage}%
+                ${item.formattedUSDValue}
               </p>
               <p className="text-primary-400 font-bold text-xs mt-1 leading-none">
-                {item.amount} {item.name}
+                {formatEther(item.amount)} {item.name}
               </p>
             </div>
           </li>
         ))}
       </ul>
       <div className="flex space-x-2 border-box">
-        <Button className="mt-8 font-black h-11 w-11 bg-white border-b-4">
+        <Button className="mt-8 px-4 !w-11" variant={'outline'} size={'lg'}>
           <span className="text-primary-700 text-xl">-</span>
         </Button>
-        <Button className="mt-8 w-fit font-black border-b-4 rounded-2xl h-11 w-full">
+        <Button className="mt-8 w-fit border-b-4 h-11 w-full" size={'lg'}>
           Buy 1 Box <span className="text-primary-100">$1.2</span>
         </Button>
-        <Button className="mt-8 font-black h-11 w-11 bg-white border-b-4">
+        <Button className="mt-8 px-4 w-11" variant={'outline'} size={'lg'}>
           <span className="text-primary-700 text-xl">+</span>
         </Button>
       </div>
